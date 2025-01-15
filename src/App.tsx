7140f811a01ad436b52296cedf48dc7637b21245
Greg Trihus <gtryus@gmail.com>
2025-01-15 14:47:09 -0600

    release 2.4.0 check boxes for testing filters, noScript and noFont

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,23 @@ import {
   getFamily,
 } from "mui-language-picker";
 import React from "react";
-import { Badge, Box, Grid, Link, Stack, Typography } from "@mui/material";
+import {
+  Badge,
+  Box,
+  Checkbox,
+  FormControlLabel,
+  Grid,
+  Link,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { StyledTextAreaAudosize } from "./WebFontStyles";
 import { getFontUrl } from "./getFontUrl";
 import { version } from "../package.json";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { isDark } from "./isDark";
-// import MmsData from "./mui-language-picker/data/mms-lang";
+import MmsData from "./assets/mmsLangs";
 
 const darkTheme = createTheme({
   palette: {
@@ -31,7 +40,10 @@ function App() {
   const [fontName, setFontName] = React.useState("");
   const [fontUrl, setFontUrl] = React.useState("");
   const [rtl, setRtl] = React.useState(false);
-  // const mmsLang = React.useRef<Set<string>>(new Set());
+  const mmsLang = React.useRef<Set<string>>(new Set());
+  const [filtered, setFiltered] = React.useState(false);
+  const [noScript, setNoScript] = React.useState(false);
+  const [noFont, setNoFont] = React.useState(false);
   const [tagData, setTagData] = useState<JSX.Element | null>(null);
   const [familyData, setFamilyData] = useState<JSX.Element | null>(null);
 
@@ -68,11 +80,13 @@ function App() {
     return tag?.localname ? `${tag?.localname} / ${name}` : tag?.name || name;
   };
 
-  // const handleFilter = (code: string) => {
-  //   const tag = getLangTag(code);
-  //   const iso639_3 = tag?.iso639_3;
-  //   return mmsLang.current.has(iso639_3 ?? "und");
-  // }
+  const handleFilter = (code: string) => {
+    const tag = getLangTag(code);
+    if (['Zxxx', 'Sgnw', 'Brai'].includes(tag?.script ?? '')) return false;
+    if (tag?.tag?.split('-')?.[0] === 'zh') return true;
+    const iso639_3 = tag?.iso639_3;
+    return mmsLang.current.has(iso639_3 ?? "und");
+  };
 
   React.useEffect(() => {
     const tag = getLangTag(bcp47);
@@ -87,17 +101,19 @@ function App() {
     );
   }, [fontName]);
 
-  // React.useEffect(() => {
-  //     mmsLang.current = new Set(MmsData.filter(i => i.is_mms_asr === true).map(d => d.iso));
-  //     console.log(
-  //       `${mmsLang.current.size} unique mms asr langs loaded, ${MmsData.length} total`
-  //     );
-  //   }, []);
+  React.useEffect(() => {
+    mmsLang.current = new Set(
+      MmsData.filter(i => i.is_mms_asr === true).map(d => d.iso)
+    );
+    console.log(
+      `${mmsLang.current.size} unique mms asr langs loaded, ${MmsData.length} total`
+    );
+  }, []);
 
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <div className="App">
+      <div className="App" style={{ width: "100%" }}>
         <Grid container spacing={2}>
           <Grid item xs={4}>
             <pre>
@@ -125,52 +141,83 @@ function App() {
               </h2>
             </Box>
             <div className="card">
-              <LanguagePicker
-                value={bcp47}
-                setCode={handleBcp47}
-                name={lgName}
-                setName={handleName}
-                font={fontName}
-                setFont={handleFontName}
-                setDir={handleRtl}
-                setInfo={handleInfo}
-                displayName={displayName}
-                // filter={handleFilter}
-                required
-                t={languagePickerStrings_en}
-              />
-              {fontName && (
-                <Stack spacing={1} sx={{ m: 2 }}>
-                  <Badge
-                    badgeContent={
-                      <Link
-                        href="https://keyman.com/"
-                        target="_blank"
-                        color="inherit"
-                      >
-                        i
-                      </Link>
-                    }
-                    color="primary"
-                    sx={{ alignSelf: "center" }}
-                  >
-                    <Typography variant="h6" component="span">
-                      Type some text here:
-                    </Typography>
-                  </Badge>
-                  <StyledTextAreaAudosize
-                    family={fontName}
-                    url={fontUrl}
-                    lang={bcp47 || "und"}
-                    sx={{
-                      p: 1,
-                      direction: rtl ? "rtl" : "ltr",
-                      fontFamily: fontName,
-                      fontSize: "x-large",
-                    }}
-                  />
-                </Stack>
-              )}
+              <Stack>
+                <LanguagePicker
+                  value={bcp47}
+                  setCode={handleBcp47}
+                  name={lgName}
+                  setName={handleName}
+                  font={fontName}
+                  setFont={handleFontName}
+                  setDir={handleRtl}
+                  setInfo={handleInfo}
+                  displayName={displayName}
+                  filter={filtered? handleFilter: undefined}
+                  required
+                  noScript={noScript}
+                  noFont={noFont}
+                  t={languagePickerStrings_en}
+                />
+                {fontName && (
+                  <Stack spacing={1} sx={{ m: 2 }}>
+                    <Badge
+                      badgeContent={
+                        <Link
+                          href="https://keyman.com/"
+                          target="_blank"
+                          color="inherit"
+                        >
+                          i
+                        </Link>
+                      }
+                      color="primary"
+                      sx={{ alignSelf: "center" }}
+                    >
+                      <Typography variant="h6" component="span">
+                        Type some text here:
+                      </Typography>
+                    </Badge>
+                    <StyledTextAreaAudosize
+                      family={fontName}
+                      url={fontUrl}
+                      lang={bcp47 || "und"}
+                      sx={{
+                        p: 1,
+                        direction: rtl ? "rtl" : "ltr",
+                        fontFamily: fontName,
+                        fontSize: "x-large",
+                      }}
+                    />
+                  </Stack>
+                )}
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={filtered}
+                      onChange={(_e, checked) => setFiltered(checked)}
+                    />
+                  }
+                  label="Apply MMS filter"
+                />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={noScript}
+                      onChange={(_e, checked) => setNoScript(checked)}
+                    />
+                  }
+                  label="No script selection"
+                />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={noFont}
+                      onChange={(_e, checked) => setNoFont(checked)}
+                    />
+                  }
+                  label="No font selection"
+                />
+              </Stack>
             </div>
           </Grid>
           <Grid item xs={3}>
